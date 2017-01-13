82817e6a3e9bf8b46eb53a0ae23eccc11528af8f
Anand Singh <anand9522s@gmail.com>
2017-01-13 10:29:06 +0530

    some changes

---
+++ src/Twitter_Analysis/Cluster.java
@@ -0,0 +1,7 @@
+package Twitter_Analysis;
+
+/**
+ * Created by anand on 13/01/17.
+ */
+public class Cluster {
+}
---
+++ src/Twitter_Analysis/TwitterGraph.java
@@ -0,0 +1,20 @@
+package Twitter_Analysis;
+
+import java.util.HashMap;
+
+/**
+ * Created by anand on 12/01/17.
+ */
+public class TwitterGraph {
+
+    //int corresponding to GraphNode
+    private HashMap<Integer,GraphNode> graphNodeMap;
+
+    public TwitterGraph(){
+        graphNodeMap=new HashMap<>();
+    }
+    //
+    public void addEdge(){
+
+    }
+}
