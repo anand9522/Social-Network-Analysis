19341a9db3c5cb8f10e41910609e4b4cb476af6f
Anand Singh <anand9522s@gmail.com>
2017-01-13 15:29:00 +0530

    30% work done

--- src/Twitter_Analysis/Cluster.java
+++ src/Twitter_Analysis/Cluster.java
@@ -1,7 +1,17 @@
 package Twitter_Analysis;
 
+import java.util.HashSet;
+
 /**
  * Created by anand on 13/01/17.
  */
 public class Cluster {
+    private HashSet<GraphNode> allotedVertices;
+
+    public Cluster() {
+        allotedVertices=new HashSet<>();
+    }
+
+
+
 }
--- src/Twitter_Analysis/TwitterGraph.java
+++ src/Twitter_Analysis/TwitterGraph.java
@@ -1,6 +1,9 @@
 package Twitter_Analysis;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by anand on 12/01/17.
@@ -16,5 +19,23 @@ public class TwitterGraph {
     //
     public void addEdge(){
 
+    }
+
+    public HashSet<Cluster> getCommunities(int user, int k){
+        if(graphNodeMap.get(user).getFollowers().size()<k){
+            return null;
+        }
+        HashSet<Cluster> clusters=new HashSet<>();
+        randomClusterAllotment(clusters,user,k);
+
+        return clusters;
+    }
+
+    public void randomClusterAllotment(HashSet<Cluster> clusters, int user, int k){
+        HashSet<GraphNode> alloted_centers=new HashSet<>();
+        Set<GraphNode> user_followers=new TreeSet<>(graphNodeMap.get(user).getFollowers());
+        for (int i = 0; i < k; i++) {
+            Cluster c=new Cluster();
+        }
     }
 }
